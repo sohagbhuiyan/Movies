2d2db636618deecab15ca9970458ecb84604f6c6
sohagbhuiyan <smsohag171@gmail.com>
2024-12-24 21:48:08 +0600

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by macmini on 12/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
